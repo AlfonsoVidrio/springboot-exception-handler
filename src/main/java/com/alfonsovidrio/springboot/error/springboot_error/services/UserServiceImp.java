570df24c73befdc785c506f94cc46376700a2303
Alfonso Vidrio <154586838+AlfonsoVidrio@users.noreply.github.com>
2025-02-26 08:48:25 -0600

    feat: implement UserService with methods to find all users and find by ID

---
+++ src/main/java/com/alfonsovidrio/springboot/error/springboot_error/services/UserServiceImp.java
@@ -0,0 +1,26 @@
+package com.alfonsovidrio.springboot.error.springboot_error.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.alfonsovidrio.springboot.error.springboot_error.models.domain.User;
+
+@Service
+public class UserServiceImp implements UserService{
+    @Autowired
+    private List<User> users;
+
+    @Override
+    public List<User> findAll() {
+        return users;
+    }
+
+    @Override
+    public Optional<User> findById(Long id) {
+        return users.stream().filter(u -> u.getId().equals(id)).findFirst();
+    }
+
+}
